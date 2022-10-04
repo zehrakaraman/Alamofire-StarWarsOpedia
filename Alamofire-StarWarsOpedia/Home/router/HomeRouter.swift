34dfb67bcfe8a1e87773caa9fd6f2d76744f547d
Zehra Karaman <zehrakaramaan@gmail.com>
2022-10-04 23:35:30 +0300

    Fix home screen

---
+++ Alamofire-StarWarsOpedia/Home/router/HomeRouter.swift
@@ -0,0 +1,16 @@
+//
+//  HomeRouter.swift
+//  Alamofire-StarWarsOpedia
+//
+//  Created by Zehra on 4.10.2022.
+//
+
+import Foundation
+
+class HomeRouter: PresenterToRouterHomeProtocol {
+    
+    static func createModule(ref: HomeViewController) {
+        <#code#>
+    }
+    
+}
